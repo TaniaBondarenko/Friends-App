43da0d76fb40f96c8522776bd3da934647ec79de
Tetiana Bondarenko <tn.a.bondarenko@gmail.com>
2021-01-15 22:39:25 +0200

    load numOfFriends/modify gender detection

--- app.js
+++ app.js
@@ -1,49 +1,75 @@
-const urlInit = 'https://randomuser.me/api/?results=10&&?inc=gender,name,location,email,cell,picture';
+const numOfFriends=4;
+const urlInit = `https://randomuser.me/api/?results=${numOfFriends}`;
 const friendsContainer = document.querySelector(".friends_container");
-const friendCard = document.createElement("div");
-friendCard.setAttribute("class", "friend_card");
+
 let friendsData;
-let friendName;
-let friendPhoto;
-let friendLocation;
-let friendCont; 
-let friendGender;
+let fullName;
+let photo;
+let position;
+let contacts; 
+let gender;
 
-fetch(urlInit)
-        .then(response => response.json())
-        .then((data) => {
-                friendsData = data.results[0];   
-                //addFriendsDataToCard();   
-                //detectGender();
-        })
-        .then(() => addFriendsDataToCard())
-        .then(() =>detectGender())
-        .catch(err => {
-                console.log("Oops... Something went wrong.\n Reload")
-        });
+function addFriend() {
+        fetch(urlInit)
+                .then(response => response.json())
+                .then((data) => {
+                        for (i = 0; i < numOfFriends; i++) {
+                                friendsData = data.results;//array of objects
+                        }
+                })
+                .then(() => addFriendsDataToCard())
+                .catch(err => {
+                        console.log("Oops... Something went wrong.\n Reload")
+                });
+       
+        return friendsData;
 
+};
 
-
-function addFriendsDataToCard() {
-        friendName = (`${friendsData.name.first} ${friendsData.name.last}`).toUpperCase();
-        friendPhoto = friendsData.picture.large;
-        friendLocation = friendsData.location.city;
-        friendCont = `${friendsData.email}\n${friendsData.cell}`;
-        friendGender = friendsData.gender;
-        let innerText = `<div class="name">${friendName}</div>
-                                <div class="photo"><img src="${friendPhoto}"></div>
+async function addFriendsDataToCard() {
+        try {
+                let innerText; let innerTextStr = " ";
+                await friendsData.forEach(friend => {
+                        fullName = (`${friend.name.first} ${friend.name.last}`).toUpperCase();
+                        photo = friend.picture.large;
+                        position = friend.location.city;
+                        contacts = `${friend.email}\n${friend.cell}`;
+                        gender = friend.gender;
+                        innerText = `<div class="friend_card">
+                                <div class="name">${fullName}</div>
+                                <div class="photo"><img src="${photo}"></div>
                                 <div class="info_block">
-                                <div class="place">${friendLocation}</div>
-                                <div class="contacts">${friendCont}</div><hr>
-                                <div class="gender">${friendGender}</div>
-                                </div>`;
-        friendCard.innerHTML = innerText;
-       friendsContainer.appendChild(friendCard);
+                                <div class="place">${position}</div>
+                                <div class="contacts">${contacts}</div><hr>
+                                <div class="gender">${gender}</div>
+                                </div>
+                                </div>
+                                `;
+                        innerTextStr += innerText;
+                       
+                        
+                });
+                friendsContainer.innerHTML = innerTextStr;
+                console.log(friendsData);
+        } catch (error) {
+                console.log("Can't get all your friends")
+        }
 }
 
 function detectGender() {
-        if (friendGender === "female") {
-                let sex = document.querySelector(".friend_card");
-                sex.classList.add("female");
-        }
-}
+        let sex = Array.from(document.querySelectorAll(".friend_card"));
+        for (let i = 0; i < numOfFriends;i++) {
+                if (friendsData[i].gender === "female") {
+                        sex[i].classList.add("female");
+                } else {
+                        sex[i].classList.add("male");
+                }
+        };
+};
+
+
+window.addEventListener("load", addFriend);
+setTimeout(()=> detectGender(),1000);
+
+
+
