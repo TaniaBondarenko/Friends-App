78c1c319124f3b5c698954c2c49b69ce55755af6
Tetiana Bondarenko <tn.a.bondarenko@gmail.com>
2021-01-18 22:52:26 +0200

    add filter rewrite addFriends function

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
-const numOfFriends=4;
+const numOfFriends=10;
 const urlInit = `https://randomuser.me/api/?results=${numOfFriends}`;
 const friendsContainer = document.querySelector(".friends_container");
-
+const friendCard = document.querySelectorAll(".friend_card");
 let friendsData;
 let fullName;
 let photo;
 let position;
 let contacts; 
 let gender;
+let innerText = " "; 
 
-function addFriend() {
+function initState() {
+        friendsContainer.innerHTML=innerText.replace(innerText," ");
+}
+
+function fetchFriend() {
         fetch(urlInit)
                 .then(response => response.json())
                 .then((data) => {
+                         
                         for (i = 0; i < numOfFriends; i++) {
-                                friendsData = data.results;//array of objects
+                                friendsData = data.results;//array of objects 
                         }
+                        console.log(friendsData);
                 })
-                .then(() => addFriendsDataToCard())
+                .then(() => addFriends(friendsData))
+                .then(() => detectGender(friendsData))
                 .catch(err => {
                         console.log("Oops... Something went wrong.\n Reload")
                 });
@@ -26,40 +34,28 @@ function addFriend() {
 
 };
 
-async function addFriendsDataToCard() {
-        try {
-                let innerText; let innerTextStr = " ";
-                await friendsData.forEach(friend => {
-                        fullName = (`${friend.name.first} ${friend.name.last}`).toUpperCase();
-                        photo = friend.picture.large;
-                        position = friend.location.city;
-                        contacts = `${friend.email}\n${friend.cell}`;
-                        gender = friend.gender;
-                        innerText = `<div class="friend_card">
-                                <div class="name">${fullName}</div>
-                                <div class="photo"><img src="${photo}"></div>
+async function addFriends(arr) {
+        let fragment = document.createDocumentFragment();
+        await arr.forEach(friend => {
+                let friendCard = document.createElement("div");
+                friendCard.setAttribute("class", "friend_card");
+                friendCard.innerHTML = `<div class="name">${(`${friend.name.first} ${friend.name.last}`).toUpperCase()}</div>
+                                <div class="photo"><img src="${friend.picture.large}"></div>
                                 <div class="info_block">
-                                <div class="place">${position}</div>
-                                <div class="contacts">${contacts}</div><hr>
-                                <div class="gender">${gender}</div>
-                                </div>
+                                <div class="place">${friend.location.city}</div>
+                                <div class="contacts">${`${friend.email}\n${friend.cell}`}</div><hr>
+                                <div class="gender">${friend.gender}</div>
                                 </div>
                                 `;
-                        innerTextStr += innerText;
-                       
-                        
-                });
-                friendsContainer.innerHTML = innerTextStr;
-                console.log(friendsData);
-        } catch (error) {
-                console.log("Can't get all your friends")
-        }
-}
+                fragment.appendChild(friendCard);
+        });
+        friendsContainer.appendChild(fragment);
+};
 
-function detectGender() {
+function detectGender(arr) {
         let sex = Array.from(document.querySelectorAll(".friend_card"));
         for (let i = 0; i < numOfFriends;i++) {
-                if (friendsData[i].gender === "female") {
+                if (arr[i].gender === "female") {
                         sex[i].classList.add("female");
                 } else {
                         sex[i].classList.add("male");
@@ -67,9 +63,51 @@ function detectGender() {
         };
 };
 
+const sidebar = document.querySelector(".sidebar");
+sidebar.addEventListener("click", clickButton);
 
-window.addEventListener("load", addFriend);
-setTimeout(()=> detectGender(),1000);
+
+function clickButton({ target }) {
+        switch (target.className||target.id) {
+                case "ascendent_by_name":
+                        console.log("click");
+                        break;
+                case "descendent_by_name":
+                console.log("click,click");
+                        break;
+                case "ascendent_by_age":
+                        console.log("click age");
+                        break;
+                case "descendent_by_age":
+                        console.log("click click age");
+                        break;
+                case "male":
+                case "female":
+                case "all":
+                        letFilter();
+                        break;
+
+        }
+
+}
+
+function letFilter() {
+        initState();
+        if (document.getElementById("male").checked) {
+                const justMale = friendsData.filter(el => el.gender === "male");
+                addFriends(justMale);
+        } else if (document.getElementById("female").checked) {
+                const justFemale = friendsData.filter(el => el.gender === "female");
+                addFriends(justFemale);
+        } else if (document.getElementById("all").checked) {
+                addFriends(friendsData);
+        }
+}
+
+
+
+window.addEventListener("load", fetchFriend);
+//setTimeout(()=> detectGender(),1000);
 
 
 
