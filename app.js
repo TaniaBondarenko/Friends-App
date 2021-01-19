6b64772d95a217661d07468220956af4ab23a687
Tetiana Bondarenko <tn.a.bondarenko@gmail.com>
2021-01-19 13:53:21 +0200

    add search function

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ let friendsData;
 let fullName;
 let photo;
 let position;
-let contacts; 
+let email; 
 let gender;
+let age;
 let innerText = " "; 
 
 function initState() {
@@ -22,10 +23,8 @@ function fetchFriend() {
                         for (i = 0; i < numOfFriends; i++) {
                                 friendsData = data.results;//array of objects 
                         }
-                        console.log(friendsData);
                 })
                 .then(() => addFriends(friendsData))
-                .then(() => detectGender(friendsData))
                 .catch(err => {
                         console.log("Oops... Something went wrong.\n Reload")
                 });
@@ -38,34 +37,22 @@ async function addFriends(arr) {
         let fragment = document.createDocumentFragment();
         await arr.forEach(friend => {
                 let friendCard = document.createElement("div");
-                friendCard.setAttribute("class", "friend_card");
-                friendCard.innerHTML = `<div class="name">${(`${friend.name.first} ${friend.name.last}`).toUpperCase()}</div>
+               friendCard.setAttribute("class", "friend_card");
+                friendCard.innerHTML = `<div class="name ${friend.gender}">${(`${friend.name.first} ${friend.name.last}`).toUpperCase()}</div>
                                 <div class="photo"><img src="${friend.picture.large}"></div>
-                                <div class="info_block">
+                                <div class="info_block ${friend.gender}">
+                                <div class="age">Age ${friend.dob.age}</div>
                                 <div class="place">${friend.location.city}</div>
-                                <div class="contacts">${`${friend.email}\n${friend.cell}`}</div><hr>
+                                <div class="email"><a href="mailto:${friend.mail}">${friend.email}</a></div><hr>
                                 <div class="gender">${friend.gender}</div>
                                 </div>
                                 `;
-                fragment.appendChild(friendCard);
+                fragment.appendChild(friendCard);       
         });
         friendsContainer.appendChild(fragment);
 };
 
-function detectGender(arr) {
-        let sex = Array.from(document.querySelectorAll(".friend_card"));
-        for (let i = 0; i < numOfFriends;i++) {
-                if (arr[i].gender === "female") {
-                        sex[i].classList.add("female");
-                } else {
-                        sex[i].classList.add("male");
-                }
-        };
-};
-
-const sidebar = document.querySelector(".sidebar");
-sidebar.addEventListener("click", clickButton);
-
+document.querySelector(".sidebar").addEventListener("click", clickButton);
 
 function clickButton({ target }) {
         switch (target.className||target.id) {
@@ -86,24 +73,61 @@ function clickButton({ target }) {
                 case "all":
                         letFilter();
                         break;
-
+                
         }
 
 }
 
+let isFiltered = false;
+let justMale;
+let justFemale;
 function letFilter() {
         initState();
         if (document.getElementById("male").checked) {
-                const justMale = friendsData.filter(el => el.gender === "male");
+                justMale = friendsData.filter(el => el.gender === "male");
                 addFriends(justMale);
+                isFiltered = true;
+
         } else if (document.getElementById("female").checked) {
-                const justFemale = friendsData.filter(el => el.gender === "female");
+                justFemale = friendsData.filter(el => el.gender === "female");
                 addFriends(justFemale);
+                isFiltered = true;
         } else if (document.getElementById("all").checked) {
                 addFriends(friendsData);
+                isFiltered = false;
         }
 }
 
+async function sortByAges(arr) {
+        console.log(arr);
+        await  
+                arr.forEach(el=>el.dob.age).sort((a, b) => a.dob.age.localeCompare(b.dob.age));
+        
+        console.log(a.dob.age);
+};
+ 
+document.querySelector(".search").addEventListener("keyup", makeSearch);
+
+function makeSearch() {      
+        const searchValue = document.querySelector(".search").value.toLowerCase();
+        let arrForFilter;
+        if (isFiltered && document.getElementById("male").checked) {
+                arrForFilter = justMale;
+        } if (isFiltered && document.getElementById("female").checked) {
+                arrForFilter = justFemale;
+        } if (!isFiltered) {
+                arrForFilter = friendsData;
+        }      
+        const filteredStr = arrForFilter.filter((elem) => {
+                return (
+                        elem.name.last.toLowerCase().includes(searchValue) ||
+                        elem.name.first.toLowerCase().includes(searchValue)
+                );
+               
+        })
+        initState();
+        addFriends(filteredStr);
+}
 
 
 window.addEventListener("load", fetchFriend);
